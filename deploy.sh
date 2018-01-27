83da27af0c10d9deade915c2667ec2ec34da294b
Daniel Margolis <dan@af0.net>
2018-01-27 15:09:09 +0000

    Copy data files to production on deploy.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ export GOROOT=/usr/lib/google-golang
 export GOPATH=$PWD
 pushd .
 cd src/app
+# XXX: This is stupid.
+cp -r ../localize/data/ .
 gcloud --project sbb-status-4f4eb app deploy --quiet
 popd
 
